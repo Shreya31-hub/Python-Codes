51c5087ba6db283b230310119488af41cbee8524
Shreya31-hub <shreyashah6968@gmail.com>
2025-06-15 01:10:02 +0530

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,20 @@
+#For Loop
+print("Simple for Loops")
+for i in range(1,6):
+    print(i)
+print("\n")
+
+#Multiplication Table
+
+print("Multiplication")
+n = int(input("enter n"))
+for i in range(1,11):
+    print(f"{n} * {i} = {n * i}")
+print("\n")
+
+#Printing * pattern
+print("pattern")
+n1 = int(input("enter n"))
+for i in range(1,6):
+    print("*" * i)
+print("\n")
